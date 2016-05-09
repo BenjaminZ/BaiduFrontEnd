2da7fd2aac6dea8510d0007df96ad30d55330a40
BenjaminZ <benjaminzhang1130@gmail.com>
2016-05-09 13:10:55 +1000

    finish task20 9/5/2016

--- stageTwo/task20/js/task.js
+++ stageTwo/task20/js/task.js
@@ -9,11 +9,12 @@
  * @type {Array} array of index to numbers.
  */
 var data = [];
+var searchResult = [];
+var defaultStr = "function clearHighLights() { searchResult.map((block)=> { block.style.backgroundColor = \"#ff0000\"; }); } function addHighLights() { searchResult.map((block)=> { block.style.backgroundColor = \"#7fff00\"; }) } function search_textButtonOnClick() { clearHighLights(); var searchStr = $(\"#search_text\").val().trim(); for (var i = 0; i < data.length; i++) { if (data[i].indexOf(searchStr) > -1) { searchResult.push($(\"#output\").children()[i]); } } addHighLights(); } $(()=> { inputButtonOnClick(); });";
 
 function getInputs(input) {
     var pattern = new RegExp(/[0-9a-zA-Z\u4e00-\u9fa5]+/, "g");
-    var inputs = input.match(pattern);
-    return inputs;
+    return input.match(pattern);
 }
 function putLeft(inputs) {
     for (var i = inputs.length - 1; i >= 0; i--) {
@@ -27,6 +28,7 @@ function putRight(inputs) {
 /**
  * Push number into data.
  * @param target the event target.
+ * @param inputs
  */
 function pushStr(target, inputs) {
     if ($(target).hasClass("left")) {
@@ -46,11 +48,8 @@ function popStr(target) {
         return data.pop();
     }
 }
-function updatePop(target, num) {
-    if (isNaN(num)) {
-        return;
-    }
-    alert(num);
+function updatePop(target, str) {
+    alert(str);
     var output = $("#output");
     if ($(target).hasClass("left")) {
         output.find(":first-child").remove();
@@ -58,32 +57,50 @@ function updatePop(target, num) {
         output.find(":last-child").remove();
     }
 }
-function getNumBlock(num) {
+function getContentBlock(content) {
     var block = document.createElement("b");
     block.style.backgroundColor = "#ff0000";
-    block.style.marginLeft = "5px";
+    block.style.margin = "5px";
     block.style.padding = "5px";
     block.style.color = "white";
-    block.textContent = num;
+    block.textContent = content;
+    block.style.display = "inline-block";
     return block;
 }
 
+function updateLeftPush(input, output) {
+    var block = getContentBlock(input);
+    output.prepend(block);
+}
+function updateRightPush(input, output) {
+    var block = getContentBlock(input);
+    output.append(block);
+}
 function updatePush(target, inputs) {
-    // TODO update function
-    var num = $("#number").val().trim();
-    var block = getNumBlock(num);
     var output = $("#output");
     if ($(target).hasClass("left")) {
-        output.prepend(block);
+        for (var i = inputs.length - 1; i >= 0; i--) {
+            updateLeftPush(inputs[i], output);
+        }
     } else {
-        output.append(block);
+        inputs.map((input)=> {
+            updateRightPush(input, output);
+        })
     }
 }
 
+function exampleButtonOnClick() {
+    var inputs = getInputs(defaultStr);
+    var output = $("#output");
+    putRight(inputs);
+    inputs.map((input)=>{
+        updateRightPush(input, output);
+    })
+}
 /**
  * Call other function based on which button is clicked.
  */
-function buttonOnClick() {
+function inputButtonOnClick() {
     /**
      * Init function.
      */
@@ -101,16 +118,44 @@ function buttonOnClick() {
                     return;
                 }
                 pushStr(target, inputs);
-                // TODO
                 updatePush(target, inputs);
                 inputArea.val("");
             } else if ($(target).hasClass("pop")) {
                 var str = popStr(target);
                 updatePop(target, str);
+            } else if ($(target).hasClass("search_button")) {
+                search_textButtonOnClick();
+            } else if ($(target).hasClass("example_button")) {
+                exampleButtonOnClick();
+            } else if ($(target).hasClass("clear")) {
+                $("#input_text").val("");
             }
         }
     });
 }
+function clearHighLights() {
+    searchResult.map((block)=> {
+        block.style.backgroundColor = "#ff0000";
+    });
+    searchResult = [];
+}
+function addHighLights() {
+    searchResult.map((block)=> {
+        block.style.backgroundColor = "#7fff00";
+    })
+}
+function search_textButtonOnClick() {
+    var searchBar = $("#search_text");
+    clearHighLights();
+    var searchStr = searchBar.val().trim();
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].indexOf(searchStr) > -1) {
+            searchResult.push($("#output").children()[i]);
+        }
+    }
+    addHighLights();
+}
+
 $(()=> {
-    buttonOnClick();
+    inputButtonOnClick();
 });
