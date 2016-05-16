e8ff148f62a16251f42f2388d8ab247b36b75ab2
BenjaminZ <benjaminzhang1130@gmail.com>
2016-05-16 11:33:44 +1000

    finish task22 16/5/2016

--- stageTwo/task22/js/main.js
+++ stageTwo/task22/js/main.js
@@ -6,31 +6,84 @@
 
 var output = document.getElementsByClassName("output")[0];
 
+var divList = [];
+var timer;
+var milSeconds = 500;
+var numOfLayers = 5;
 
 function bindButtons() {
     var wrap = document.getElementsByClassName("buttons")[0];
     wrap.addEventListener("click", buttonOnClick, false);
 }
 
-function preOder() {
-    alert("pre-oder");
+function preOder(self) {
+    var s = self;
+    if (!s) {
+        return;
+    }
+    divList.push(s);
+    preOder(s.firstChild);
+    preOder(s.lastChild);
 }
 
+function oderDisplay() {
+    if (divList.length === 0) {
+        return;
+    }
+    var i = 0;
+    divList[i].style.backgroundColor = "blue";
+    timer = setInterval(()=> {
+        divList[i].style.backgroundColor = "white";
+        i++;
+        if (i >= divList.length) {
+            clearInterval(timer);
+        } else {
+            divList[i].style.backgroundColor = "blue";
+        }
+    }, milSeconds);
+}
+function initDisplay() {
+    clearInterval(timer);
+    divList.map((e)=> {
+        e.style.backgroundColor = "white";
+    });
+    divList = [];
+}
+function preOderOnClick() {
+    initDisplay();
+    preOder(output.children[0]);
+    oderDisplay();
+}
 function buttonOnClick(event) {
     var id = event.target.getAttribute("id");
     switch (id) {
         case "#pre-oder":
-            preOder();
+            preOderOnClick();
             break;
     }
 }
 
-function displayTree() {
-
+function displayTree(parent, layers) {
+    var n = layers;
+    var p = parent || output;
+    var self = document.createElement("div");
+    self.style.margin = "5px 3px";
+    self.style.border = "solid thin";
+    self.style.display = "inline-block";
+    self.style.backgroundColor = "white";
+    if (n > 1) {
+        p.appendChild(self);
+        displayTree(self, n - 1);
+        displayTree(self, n - 1);
+    } else {
+        self.style.height = "16px";
+        self.style.width = "9px";
+        p.appendChild(self);
+    }
 }
 
 (()=> {
     bindButtons();
-    displayTree();
+    displayTree(null, numOfLayers);
 })();
 
